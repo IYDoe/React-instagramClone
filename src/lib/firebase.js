19ec3bbd31fa2b7ec46af496b023333174d71962
Ilya <yudin2401@gmail.com>
2021-02-22 18:11:06 +0300

    update

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,18 +1,18 @@
+//import { seedDatabase } from '../seed';
 
-// import { seedDatabase } from '../seed'
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBKiB7_iMou71OA87grCfKvvBgkKJxz1UI",
-    authDomain: "instagram-3902a.firebaseapp.com",
-    projectId: "instagram-3902a",
-    storageBucket: "instagram-3902a.appspot.com",
-    messagingSenderId: "466446542662",
-    appId: "1:466446542662:web:466d3d46c2ee36836fe747"
+const config = {
+  apiKey: "AIzaSyBKiB7_iMou71OA87grCfKvvBgkKJxz1UI",
+  authDomain: "instagram-3902a.firebaseapp.com",
+  databaseURL: "https://instagram-3902a-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "instagram-3902a",
+  storageBucket: "instagram-3902a.appspot.com",
+  messagingSenderId: "466446542662",
+  appId: "1:466446542662:web:466d3d46c2ee36836fe747"
 };
 
-const firebase = window.firebase.initializeApp(firebaseConfig)
-const { FieldValue } = window.firebase.firestore
+const firebase = window.firebase.initializeApp(config);
+const { FieldValue } = window.firebase.firestore;
 
-// seedDatabase(firebase)
+//seedDatabase(firebase);
 
-export { firebase, FieldValue }
+export { firebase, FieldValue };
